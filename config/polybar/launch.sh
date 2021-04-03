b36555a1c18cefaea72501eff8afe5fe40f794ee
maxicraftone <max@mx-lfs.localdomain>
2021-04-03 21:25:15 +0200

    Added relevant config files

---
+++ config/polybar/launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar mybar &
+    MONITOR=$m polybar mybar2 &
+  done
+else
+  polybar mybar &
+  polybar mybar2 &
+fi
---
+++ config/polybar/scripts/weather/weather.sh
@@ -0,0 +1,1 @@
+python3 $HOME/.config/polybar/scripts/weather/weather.py -u metric -c Bonn
